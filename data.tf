926b5711869eab4a542239b66890f4c3927977ea
Fiyinfoluwa <fatuasefiyinfoluwa@gmail.com>
2023-03-18 20:14:30 +0100

    data

--- data.tf
+++ data.tf
@@ -1,13 +1,5 @@
 
-data "kubernetes_service" "nginx-service"{
-  metadata {
-    name = "nginx-ingress-controller"
-  }
 
-  depends_on = [
-    helm_release.nginx-ingress-controller
-  ]
-}
 
 data "kubectl_file_documents" "prometheus-ingress" {
     content = file("prometheus.yaml")
@@ -27,17 +19,4 @@ data "kubectl_file_documents" "docs" {
 
 data "kubectl_file_documents" "ingress" {
     content = file("sockshop-ingress.yaml")
-}
-
-data "aws_eks_cluster" "eks-cluster" {
-  name = "altschool-exam-eks-cluster"
-
-  depends_on = [
-    aws_eks_cluster.eks_cluster
-  ]
-}
-
-data "aws_eks_cluster_auth" "cluster-auth" {
-  depends_on = [aws_eks_cluster.eks_cluster]
-  name       = aws_eks_cluster.eks_cluster.name
 }
---
+++ modules/data.tf
@@ -0,0 +1,22 @@
+data "aws_eks_cluster" "eks-cluster" {
+  name = "altschool-exam-eks-cluster"
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster
+  ]
+}
+
+data "aws_eks_cluster_auth" "cluster-auth" {
+  depends_on = [aws_eks_cluster.eks_cluster]
+  name       = aws_eks_cluster.eks_cluster.name
+}
+
+data "kubernetes_service" "nginx-service"{
+  metadata {
+    name = "nginx-ingress-controller"
+  }
+
+  depends_on = [
+    helm_release.nginx-ingress-controller
+  ]
+}
