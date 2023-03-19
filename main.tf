274920e8c57f4faa312dbfba9ed382a0c2c0c4c5
Fiyinfoluwa <fatuasefiyinfoluwa@gmail.com>
2023-03-19 04:49:51 +0100

    depends-provision

--- main.tf
+++ main.tf
@@ -14,9 +14,17 @@ module "provison-deploy" {
     source = "./modules"
 }
 
-module "monitoring" {
-    source = "./manifests-monitoring"
+data "kubectl_path_documents" "docs" {
+    pattern = "./manifests-monitoring/*.yaml"
 
+    depends_on = [
+        module.provison-deploy
+    ]
+}
+
+resource "kubectl_manifest" "monitoring" {
+    for_each  = toset(data.kubectl_path_documents.docs.documents)
+    yaml_body = each.value
 }
 
 terraform {
