8224cac2b322b77d319cef255ae60b6d38652dd5
Fiyinfoluwa <fatuasefiyinfoluwa@gmail.com>
2023-03-19 02:36:40 +0100

    !

--- manifests-monitoring/monitoring.tf
+++ manifests-monitoring/monitoring.tf
@@ -7,8 +7,8 @@ terraform {
 }
 
 provider "kubectl" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster-auth.token
   load_config_file       = false
 }
